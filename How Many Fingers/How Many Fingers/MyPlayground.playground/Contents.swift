4f8d8281771815d18029ac2144c8c617f34ceb99
Heather Fenton <heatherf@heatherf-2.local>
2015-04-29 15:14:55 -0400

    Add How Many Fingers app

---
+++ How Many Fingers/How Many Fingers/MyPlayground.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+
+var a = arc4random_uniform(5)
+var b = 3
+
+if 2 == a {
+    println("Hello")
+    
+}
+
+var string = "A is \(a)"
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ How Many Fingers/How Many Fingers/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  How Many Fingers
+//
+//  Created by Heather Fenton on 4/28/15.
+//  Copyright (c) 2015 silc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textboxGuess: UITextField!
+    @IBOutlet weak var labelAnswer: UILabel!
+    
+    @IBAction func buttonClick(sender: AnyObject) {
+        var answer =  Int(arc4random_uniform(5))
+        var guess = textboxGuess.text.toInt()
+        var text = ""
+        
+        if guess <= 5 {
+            if guess == answer {
+                text = "Correct, I am holding \(answer) fingers!"
+                
+            } else {
+                text = "Incorrect, I am holding \(answer) fingers"
+            }
+            labelAnswer.text =  text
+            
+            
+        } else {
+            labelAnswer.text = "Please enter a number less than or equal to 5"
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
