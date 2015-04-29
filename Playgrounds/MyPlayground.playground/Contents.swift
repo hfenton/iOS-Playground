8be23191d4a929ef03f7cc3610079b780e8abcbe
Heather Fenton <heatherf@heatherf-2.local>
2015-04-29 15:15:53 -0400

    Add Playgrounds, this is first time using playgrounds within xcode

---
+++ Playgrounds/MyPlayground.playground/Contents.swift
@@ -0,0 +1,45 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var arr = [1,2,3,4,5]
+
+
+arr.removeRange(1...2)
+
+var sample = [1,2,3,"Heather"]
+
+
+var emptyArray:[Int]
+
+
+var dict = ["name":"heather", "gender":"female", "favorite_number":11]
+
+println(dict["name"]!)
+
+
+
+dict["hair"] = "brown"
+
+
+println(dict)
+
+
+
+var test = [2,4,6,8]
+
+test.removeAtIndex(0)
+
+test.append(10)
+
+
+var dict2 = ["name":"Heather", "favorite-color":"pink"]
+
+var name = dict2["name"]!
+var color = dict2["favorite-color"]!
+
+println("My name is \(name) and my favorite color is \(color)")
+
---
+++ Playgrounds/Playgrounds/MyPlayground.playground/Contents.swift
@@ -0,0 +1,41 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var dict = ["name":"Heather","favorite-color":"pink"]
+
+var name = dict["name"]!
+
+
+var color = dict["favorite-color"]!
+
+
+println("My name is \(name) and my favorite color is \(color)")
+
+
+
+
+
+var remainder = 9 % 2
+
+
+var z = 8 % 2
+
+
+var x = 22
+
+
+if x % 2 == 0 {
+    
+    println("Its even")
+} else {
+    println("its odd")
+    
+}
+
+
+var rand = arc4random_uniform(10)
+
