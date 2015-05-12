acc42d8a6872d591a383692fd062d62f80ad5462
Heather Fenton <hfenton@virtualhold.com>
2015-05-12 10:39:32 -0400

    Add IsItPrime

---
+++ IsItPrime/MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var number = 83789347
+var isPrime = true
+
+if number == 1 {
+    isPrime = false
+}
+
+if number != 2 && number != 1 {
+    for var i = 2; i < number; i++ {
+        if number % i == 0 {
+            isPrime = false
+            // break
+        }
+    }
+}
+
+println(isPrime)
